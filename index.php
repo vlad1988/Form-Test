60f694fa6ec527bcc8cfb1f353215298d27cb72b
Vlad <vladislafus@gmail.com>
2015-07-12 12:35:45 +0300

    Add change checkbox value

--- index.php
+++ index.php
@@ -31,20 +31,20 @@
         </nav>
 
         <div class="container" ng-controller="FormCtrl">
-            <h3>Sign up</h3> 
+            <h3>Sign up</h3>
             <hr>
             <form method="POST" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="username">Name <span class="asterisk">*</span></label>
-                    <input class="form-control" type="text" name="username" required>
+                    <input class="form-control" type="text" ng-model="username" required>
                 </div>
                 <div class="form-group">
                     <label for="password">E-mail <span class="asterisk">*</span></label>
-                    <input class="form-control" type="email" name="email" required>
+                    <input class="form-control" type="email" ng-model="email" required>
                 </div>
                 <div class="form-group">
                     <label for="password">Password <span class="asterisk">*</span></label>
-                    <input class="form-control" type="password" name="password" required>
+                    <input class="form-control" type="password" ng-model="password" required>
                 </div>
                 <div class="form-group">
                     <label for="country">Country <span class="asterisk">*</span></label>
@@ -54,13 +54,25 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="message">Message </label>
-                    <textarea class="form-control">
-                        
-                    </textarea>
+                    <label>Recieve new info on your e-mail?</label> <br/>
+                    <input type="radio" ng-model="post" value="false"/> Yes
+                    <input type="radio" ng-model="post" value="true"/> No
                 </div>
                 <div class="form-group">
-                    <input class="form-control btn btn-primary" type="button" value="Send">
+                    <label>Select your intrests</label><br/>       
+                    <div ng-repeat="item in interests">
+                        <input id="{{item}}" type="checkbox" value="{{item}}" ng-checked="selection.indexOf(item) > -1" ng-click="toggleSelection(item)" /> {{ item }} <br/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message </label>
+                    <textarea class="form-control">
+
+                    </textarea>
+                </div>
+
+                <div class="form-group">
+                    <input class="form-control btn btn-primary" type="submit" value="Send">
                 </div>
             </form>
         </div>
@@ -72,5 +84,7 @@
         <script src="assets/js/bootstrap.min.js"></script>
 
         <script src="app/app.module.js"></script>
+        <script src="app/form/formService.js"></script>
+
     </body>
 </html>
