39b803cbe428137e8301280aa6a4bae7725fd45c
Vlad <vladislafus@gmail.com>
2015-07-12 10:00:56 +0300

    First commit to create form

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html ng-app>
+    <head>
+        <title>Create User</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/style/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">User Page</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="/">Home</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <div class="container">
+            <h3>Sign up</h3>
+            <hr>
+            Lorem ipsum
+        </div>
+
+
+
+        <script src="assets/js/angular.min.js"></script>
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
