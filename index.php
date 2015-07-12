35c1337d3aa8c3542a3a3dcb2d82d56e81cf219a
Vlad <vladislafus@gmail.com>
2015-07-12 11:05:12 +0300

    Add country chose in form

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html ng-app>
+<html ng-app="MainModule">
     <head>
         <title>Create User</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="assets/style/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/style.css">
+        <script>
+            document.write('<base href="' + document.location + '" />');
+        </script>
     </head>
     <body>
 
@@ -26,10 +30,39 @@
             </div>
         </nav>
 
-        <div class="container">
-            <h3>Sign up</h3>
+        <div class="container" ng-controller="FormCtrl">
+            <h3>Sign up</h3> 
             <hr>
-            Lorem ipsum
+            <form method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="username">Name <span class="asterisk">*</span></label>
+                    <input class="form-control" type="text" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">E-mail <span class="asterisk">*</span></label>
+                    <input class="form-control" type="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password <span class="asterisk">*</span></label>
+                    <input class="form-control" type="password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <label for="country">Country <span class="asterisk">*</span></label>
+
+                    <select class="form-control" ng-model="country" ng-options="country.name for country in countries track by country.code">
+                        <option value="">-- Select a Country --</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message </label>
+                    <textarea class="form-control">
+                        
+                    </textarea>
+                </div>
+                <div class="form-group">
+                    <input class="form-control btn btn-primary" type="button" value="Send">
+                </div>
+            </form>
         </div>
 
 
@@ -38,5 +71,6 @@
         <script src="assets/js/jquery.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
 
+        <script src="app/app.module.js"></script>
     </body>
 </html>
